c0e71bbf4ad7455b5fce4cbe8fe2a626c5192aed
mkuendig <michael.kuendig@gmail.com>
2015-01-26 16:45:20 +0100

    Change export method

--- client/employees/employees.js
+++ client/employees/employees.js
@@ -1,6 +1,6 @@
 Template.employees.helpers({
     employees: function() {
-        return Employees.find({},{
+        return Employees.find({}, {
             sort: {
                 name: 1
             }
@@ -20,6 +20,13 @@ Template.employees.events({
 
     'click .remove-employee': function(event) {
         var employeeId = $(event.target).attr('data-employee-id');
+        $('#remove-button').attr('data-employee-id', employeeId);
+        $('#remove-modal').modal();
+    },
+
+    'click #remove-button': function() {
+        var employeeId = $('#remove-button').attr('data-employee-id');
         Employees.remove(employeeId);
+        $('#remove-modal').modal('hide');
     }
 });
--- client/projects/projects.js
+++ client/projects/projects.js
@@ -24,8 +24,20 @@ Template.projects.events({
         document.getElementById('new-project-form').reset();
     },
 
+    'click .export-project': function(event) {
+        var projectId = $(event.target).attr('data-project-id');
+        Exporter.exportProject(projectId);
+    },
+
     'click .remove-project': function(event) {
         var projectId = $(event.target).attr('data-project-id');
+        $('#remove-button').attr('data-project-id', projectId);
+        $('#remove-modal').modal();
+    },
+
+    'click #remove-button': function() {
+        var projectId = $('#remove-button').attr('data-project-id');
         Projects.remove(projectId);
+        $('#remove-modal').modal('hide');
     }
 });
