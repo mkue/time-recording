837a05ee2d6dbedba18cccdef48a87cc71cbb1a6
mkuendig <michael.kuendig@gmail.com>
2015-02-13 16:35:55 +0100

    Startup script

--- server/startup.js
+++ server/startup.js
@@ -17,13 +17,14 @@ var stopRunningProjects = function() {
 
 Meteor.startup(function() {
     var now = new Date();
-    var millisUntil11 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 23, 0, 0, 0) - now;
-    if (millisUntil11 < 0) {
-        millisUntil11 += 86400000; // it's after 23:00, try 23:00 tomorrow.
+    var millisUntil23 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 23, 0, 0, 0) - now;
+    if (millisUntil23 < 0) {
+        millisUntil23 += 86400000; // it's after 23:00, try 23:00 tomorrow.
     }
     setTimeout(function() {
+        stopRunningProjects();
         setInterval(stopRunningProjects, 86400000);
-    }, millisUntil11);
+    }, millisUntil23);
 });
 
 Meteor.methods({
